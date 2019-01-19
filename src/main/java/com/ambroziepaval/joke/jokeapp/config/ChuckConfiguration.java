bc4529cd8db46647700d04aaab7afbaec8c3245c
AmbroziePaval <ambroziepaval@gmail.com>
2019-01-19 14:22:55 +0200

    Extract ChuckNorrisQuotes to a Java configuration file.

---
+++ src/main/java/com/ambroziepaval/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package com.ambroziepaval.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Ambrozie on 19/01/2019
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
